db086134bb1c412f95ba3838f0ff70a945ed59a0
Thanasak <Thanasaksrisaeng@gmail.com>
2021-03-16 22:50:35 +0700

    update

--- main/pharmacy.php
+++ main/pharmacy.php
@@ -1,11 +1,13 @@
-<!-- <?php
+<?php
 include("../include/connection.php");
-?> -->
+?>
+
+
 <!doctype html>
 <html lang="en">
 <head>
     <title>pharmacy</title>
-    <!-- Required meta tags -->
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
@@ -14,7 +16,7 @@ include("../include/connection.php");
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css'>
     <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-    <!-- Bootstrap CSS -->
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
@@ -25,8 +27,6 @@ include("../include/connection.php");
         <div class = "container"><br>              
         <h3>ยา (ผลิตภัณฑ์)</h3>
     <form class="form" action="userRegisterInsert.php" method="POST">
-        <input type="hidden" name="lineId" id="lineId" value="<?php echo (isset($_GET['lineId']))?$_GET['lineId']:"";?>" >
-        
         <div class="input-group">
             <input type="text" class="form-control" placeholder="Input group example" aria-label="Input group example" aria-describedby="btnGroupAddon2">
 
@@ -100,62 +100,29 @@ include("../include/connection.php");
     </form>
 </div>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>  
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src ="https://www.jquery-az.com/boots/js/validate-bootstrap/validate-bootstrap.jquery.min.js" ></script>
 
 
     <script>
-        // var settings = {
-        //     "url": "http://porta.fda.moph.go.th/FDA_SEARCH_ALL/WS_LICENSE_SEARCH.asmx?WSDL",
-        //     "method": "POST",
-        //     "timeout": 0,
-        //     "headers": {
-        //         "Content-Type": "text/xml",
-        //         header('Access-Control-Allow-Origin: *')
-        //     },
-        //     "data": "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \r\nxmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" \r\nxmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n    <soap:Body>\r\n        <GET_DATA_ALL xmlns=\"http://tempuri.org/\">\r\n            <DATAS>1016200023984</DATAS>\r\n        </GET_DATA_ALL>\r\n    </soap:Body>\r\n</soap:Envelope>",
-        // };
-        // $.ajax(settings).done(function (response) {
-        //     console.log(response);
-        // });
-
-        // $(function(){
-        //     $("#btnGroupAddon2").click(function(){
-        //         alert("aaaaaa");
-        //         $.ajax({
-        //             method: "POST",
-        //             url: "getAmpur.php",
-        //             data: { provinceCode: provinceCode}
-        //         }).done(function( msg ) {
-        //             $("#div-districtCode").html(msg);
-        //             let tambonmsg= '<label for="subdistrictCode">ตำบล</label>';
-        //                 tambonmsg+='<select name="subdistrictCode" id="subdistrictCode" class="form-control" required data-error-msg="กรุณากรอกชื่อตำบล">';
-        //                 tambonmsg+='<option selected disabled>Choose...</option>';
-        //                 tambonmsg+='</select>';            
-        //             $("#div-subdistrictCode").html(tambonmsg);
-        //         });
-        //     })
-        // })
-
-
-        
-    // var settings = {
-    //     "url": "http://porta.fda.moph.go.th/FDA_SEARCH_ALL/WS_LICENSE_SEARCH.asmx?WSDL",
-    //     "method": "POST",
-    //     "timeout": 0,
-    //     "headers": {
-    //         "Content-Type": "text/xml",
-    //         "Cookie": "cookiesession1=678B769A89012BEILORVYBEFGHIJE48F"
-    // },
-    //   "data": "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \r\nxmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" \r\nxmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n    <soap:Body>\r\n        <GET_DATA_ALL xmlns=\"http://tempuri.org/\">\r\n            <DATAS>1016200023984</DATAS>\r\n        </GET_DATA_ALL>\r\n    </soap:Body>\r\n</soap:Envelope>",
-    // };
-
-    // $.ajax(settings).done(function (response) {
-    //   console.log(response);
-    // });
+        $(function(){
+            $("#btnGroupAddon2").click(function(){
+                alert("aaaaaa");
+                $.ajax({
+                    method: "POST",
+                    url: "getAmpur.php",
+                    data: { provinceCode: provinceCode}
+                }).done(function( msg ) {
+                    $("#div-districtCode").html(msg);
+                    let tambonmsg= '<label for="subdistrictCode">ตำบล</label>';
+                        tambonmsg+='<select name="subdistrictCode" id="subdistrictCode" class="form-control" required data-error-msg="กรุณากรอกชื่อตำบล">';
+                        tambonmsg+='<option selected disabled>Choose...</option>';
+                        tambonmsg+='</select>';            
+                    $("#div-subdistrictCode").html(tambonmsg);
+                });
+            })
+        });
 
     </script>
 
